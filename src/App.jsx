38f35cc91016dcf010f1920c5cb0b9d5bd740b44
craystl <chooicrystal@gmail.com>
2026-05-14 18:22:07 +1200

    website

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>Pub Quiz Buster</h1>
+        <p>A quick quiz game to test your general knowledge.</p>
+        <p>Answer questions, track your score, and challenge your friends.</p>
+        <p>Play Pub Quiz Buster to test your general knowledge!</p>
+        
+
+        //figure out if we can link our original html files, so we don't have to rewrite all the content as jsx
+        
+    </div>
+  );
+
+}
+
+export default App
+
