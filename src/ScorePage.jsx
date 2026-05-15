4f4a2a2c5e6a0ac45e532c69e016f9fa5f198caf
craystl <chooicrystal@gmail.com>
2026-05-15 15:27:47 +1200

    Add ScorePage component with score display

---
+++ src/ScorePage.jsx
@@ -0,0 +1,73 @@
+function ScorePage() {
+  return (
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        padding: "40px",
+        height: "90vh",
+        position: "relative",
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "40px",
+        }}
+      >
+        Score
+      </h1>
+
+      <div
+        style={{
+          background: "#ADD8E6",
+          width: "500px",
+          height: "300px",
+          margin: "40px auto",
+          borderRadius: "10px",
+        }}
+      ></div>
+
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "30px",
+          marginTop: "20px",
+        }}
+      >
+        Total Score: 200 / 600
+      </p>
+
+      <button
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          left: "20px",
+          background: "black",
+          color: "white",
+          padding: "12px 25px",
+          fontSize: "20px",
+          border: "none",
+        }}
+      >
+        Exit
+      </button>
+
+      <button
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          background: "black",
+          color: "white",
+          padding: "12px 25px",
+          fontSize: "20px",
+          border: "none",
+        }}
+      >
+        Back to Home
+      </button>
+    </div>
+  );
+}
+
+export default ScorePage;
