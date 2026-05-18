7e28abc3fe73c3788a6899c2e61c4c9ca8307ed2
Alexandra <alexandratoall@gmail.com>
2026-05-18 16:15:59 +1200

    update

--- APIClient.js
+++ APIClient.js
@@ -2,7 +2,7 @@ import defaultQuestions from "./questions.json";
 
 // Gets questions based on filter params from the default questions.json
 export const fetchQuestions = (params) => {
-  const { topic, gameType, questionCount = 10 } = params;
+  const { topic, gameType, questionCount } = params; 
 
   // Check that the topic exists
   if (!defaultQuestions[topic]) {
@@ -14,20 +14,27 @@ export const fetchQuestions = (params) => {
     throw new Error(`Game type "${gameType}" not found for topic "${topic}"`);
   }
 
+  // Grab the questions for our working pool
   const pool = defaultQuestions[topic][gameType];
 
+  // If there are no questions in our pool
   if (pool.length === 0) {
     throw new Error(`No questions found for ${topic} - ${gameType}`);
   }
 
+  // Shallow copy the pool so we don't affect the actual data
   return [...pool]
+    // Sort them
     .sort(() => Math.random() - 0.5)
+    // Take as many questions as requested 
     .slice(0, Math.min(questionCount, pool.length));
 };
 
+// Get available options
 export const getAvailableOptions = () => {
   const available = {};
 
+  // Returns an object containing of only topics and games that have questions in them
   for (const topic of Object.keys(defaultQuestions)) {
     const gameTypes = Object.keys(defaultQuestions[topic]).filter(
       (gt) => defaultQuestions[topic][gt]?.length > 0
